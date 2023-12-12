91babc25261342929cbc6b2913be5f30861688f3
pomme <copacabana@gmail.com>
2023-12-12 14:21:27 -0800

    adding part 1

---
+++ 1201.py
@@ -0,0 +1,43 @@
+f = open("1201input.txt", "r")
+
+NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+STRING_NUMS = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+    }
+
+
+def calibrate_line(line, nums):
+    first = ""
+    last = ""
+
+    for i in line:
+        if i in s and first == "":
+            first = i
+            last = i
+        elif i in s:
+            last = i
+
+    return int(first + last)
+
+
+def total(file, nums):
+
+    total = 0
+
+    for i in file:
+        total = total + calibrate_line(i, nums)
+    
+    return total
+
+
+print(total(f, nums))
+
