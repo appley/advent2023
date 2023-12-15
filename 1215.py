81fb6f6dbea2f81d626297d0074d6fe4cbe65135
pomme <copacabana@gmail.com>
2023-12-15 14:22:50 -0800

    pushing

--- 1215.py
+++ 1215.py
@@ -1,6 +1,6 @@
 f = open("input/1215.txt", "r")
 
-TEST = ['rn=1' , 'cm-', 'qp=3', 'cm=2', 'qp-', 'pc=4', 'ot=9', 'ab=5', 'pc-', 'pc=6', 'ot=7']
+TEST = ['rn=1'] # , 'cm-', 'qp=3', 'cm=2', 'qp-', 'pc=4', 'ot=9', 'ab=5', 'pc-', 'pc=6', 'ot=7']
 
 # part 1
 def to_input_list(f):
@@ -49,51 +49,19 @@ def focal_length(chars):
     return (hash(s[0]), ((s[0]), int(s[1])))
 
 
-# print(get_box('qp-'))
-
-# print(focal_length('rn=1'))
-
-# def insert_focal_lens(lens, box):
-
-#     if all(i == 0 for i in box):
-#         box.insert(0, lens)
-#         return box
-#     else:
-#         for i, j in enumerate(box):
-#             if j == 0:
-#                 continue
-#             else:
-#                 box.insert(i, lens)
-#                 return box
-            
-
-# def insert_dash_lens(box, lens):
-
-#     if lens[1] in box[(lens[0])]:
-#         box[lens[0]] = 0
-#         for i, j in enumerate(box):
-#             if box(i+1) == 0:
-#             else:
-#                 box[i+1] = j
-#             else:
-#                 return box
-#     else:
-#         return box
-    
-
 def slide_lenses(box):
 
     box.reverse()
     for i, j in enumerate(box):
-        if j == 0:
+        if j == "":
             continue
         else:
             curr = i
             prev = curr - 1
 
-            while curr > 0 and box[prev] == 0:
+            while curr > 0 and box[prev] == "":
                 box[prev] = j
-                box[curr] = 0
+                box[curr] = ""
                 curr = curr - 1
                 prev = curr - 1
     
@@ -102,25 +70,25 @@ def slide_lenses(box):
 
 TEST_BOX = [0, "rn=1", 0, 0, "abc", 0, 0]
 
-print(slide_lenses(TEST_BOX))
-
 
 def insert_lens(lens, box):
 
     if all(i == 0 for i in box):
         box[0] = lens
+        return box
     else:
         for i, j in enumerate(box):
-            while j == 0:
+            print("examining index ", i, j)
+            if j == 0:
                 continue
-            
-
-
+            else:
+                box[i-1] = lens
+                return box
 
 
 def organize_lenses(input_list):
 
-    boxes = [0] * 256
+    boxes = [""] * 256
     # i will be raw chars
     for i in input_list:
 
@@ -128,23 +96,23 @@ def organize_lenses(input_list):
             # box: label, focal length
             # (0, ('rn', 1))
             inst = focal_length(i)
-            if inst[1][1] in boxes[inst[0]]:
+            if inst[1][0] in boxes[inst[0]]:
                 boxes[inst[0]] = i
             else:
-                boxes = insert_lens(i)
+                boxes = insert_lens(i, boxes)
 
         # handle box shifting
         else:
             # box: label
             # (1, 'qp')
             inst = get_box_id(i)
-            if inst[1] in boxes[i]:
-                boxes[i] = 0
+            if inst[1] in boxes[inst[0]]:
+                boxes[inst[0]] = ""
                 boxes = slide_lenses(boxes)
 
     return boxes
 
-# print(organize_lenses("abc"))
+print(organize_lenses(TEST))
 
 
 
