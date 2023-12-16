555e73511b66df6098d62365d9973a750f86e5b0
pomme <copacabana@gmail.com>
2023-12-15 19:35:17 -0800

    WIP

--- 1201.py
+++ 1201.py
@@ -2,18 +2,6 @@ input_one = open("1201input.txt", "r")
 
 NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-STRING_NUMS = {
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9"
-    }
-
 
 def calibrate_line(line):
     first = ""
