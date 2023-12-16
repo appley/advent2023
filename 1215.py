c0a292cc3da4c90f2d2f9a490b69e193859fbca1
pomme <copacabana@gmail.com>
2023-12-15 17:51:45 -0800

    replace file

--- 1215.py
+++ 1215.py
@@ -1,8 +1,8 @@
 f = open("input/1215.txt", "r")
 
-TEST = ['rn=1'] # , 'cm-', 'qp=3', 'cm=2', 'qp-', 'pc=4', 'ot=9', 'ab=5', 'pc-', 'pc=6', 'ot=7']
+TEST = ['rn=1', 'cm-', 'qp=3',  'cm=2', 'qp-', 'pc=4', 'ot=9', 'ab=5', 'pc-', 'pc=6', 'ot=7']
 
-# part 1
+
 def to_input_list(f):
 
     l = []
@@ -13,6 +13,8 @@ def to_input_list(f):
     return [item for row in l for item in row]
 
 
+# part 1
+####
 def hash(chars):
 
     curr = 0
@@ -34,93 +36,70 @@ def total(input_list):
 
 
 # part 2
-
+####
 def get_box_id(chars):
-
     s = chars.split("-")[0]
-
     return (hash(s), s)
 
 
 def focal_length(chars):
-
     s = chars.split("=")
-
     return (hash(s[0]), ((s[0]), int(s[1])))
 
 
 def handle_focal_length(inst,  box):
 
     for i, j in enumerate(box):
-        print("hi", inst[1], j)
-        if inst[1] in j[1]:
+        if inst[1] in j:
             box[i] = ""
             cp = box.copy()
             box = slide_lenses(cp)
         return box
-            
         
 
->>>>>>> Stashed changes
 def slide_lenses(box):
-
-    box.reverse()
+            
     for i, j in enumerate(box):
         if j == "":
             continue
         else:
             curr = i
             prev = curr - 1
-
-            while curr > 0 and box[prev] == "":
+            while curr != 0 and box[prev] == "":
                 box[prev] = j
                 box[curr] = ""
                 curr = curr - 1
-                prev = curr - 1
-    
-    box.reverse()
+                prev = curr - 1  
     return box
 
-TEST_BOX = [0, "rn=1", 0, 0, "abc", 0, 0]
 
+# TEST_BOX = ["", "rn=1", "acd", "", "abc", "", ""]
 
-def insert_lens(lens, box):
+# print(handle_focal_length("rn=1", TEST_BOX))
 
-<<<<<<< Updated upstream
-    if all(i == 0 for i in box):
-        box[0] = lens
-=======
+def insert_lens(lens, inst, box):
+
     if all(i == "" for i in box):
-        box[0] = inst
->>>>>>> Stashed changes
+        box[0] = lens
         return box
+    
     else:
         for i, j in enumerate(box):
-<<<<<<< Updated upstream
-            print("examining index ", i, j)
-            if j == 0:
-                continue
-            else:
-                box[i-1] = lens
-                return box
-
-=======
-            if inst[1][0] in j[1][0]:
-                box[i] = inst
+            if inst[1][0] in j:
+                box[i] = lens
                 cp = box.copy()
                 box = slide_lenses(cp)
                 
                 return box
     
-        box.append(inst)
+        box.append(lens)
         cp = box.copy()
         box = slide_lenses(cp)
         return box
->>>>>>> Stashed changes
 
 def organize_lenses(input_list):
 
-    boxes = [""] * 256
+    boxes = [[""]] * 256
     # i will be raw chars
     for i in input_list:
 
@@ -128,35 +107,43 @@ def organize_lenses(input_list):
             # box: label, focal length
             # (0, ('rn', 1))
             inst = focal_length(i)
-            if inst[1][0] in boxes[inst[0]]:
-                boxes[inst[0]] = i
-            else:
-                boxes = insert_lens(i, boxes)
+            box_state = boxes[inst[0]].copy()
+            boxes[inst[0]] = insert_lens(i, inst, box_state)
 
         # handle box shifting
         else:
             # box: label
             # (1, 'qp')
             inst = get_box_id(i)
-            if inst[1] in boxes[inst[0]]:
-                boxes[inst[0]] = ""
-                boxes = slide_lenses(boxes)
+            box_state = boxes[inst[0]].copy()
+            boxes[inst[0]] = handle_focal_length(inst, box_state)
 
     return boxes
 
-print(organize_lenses(TEST))
+
+def total_power(boxes):
+
+    total = 0
+
+    for box_id, box in enumerate(boxes):
+        for slot, lens in enumerate(box):
+            if lens != '':
+                power = (box_id + 1) * (slot + 1) * focal_length(lens)[1][1]
+                total = total + power
+        print("totalling: ", box_id, box, total)
+
+    return total
 
 
-<<<<<<< Updated upstream
-=======
-# def power(box, slot, fl)
+l = to_input_list(f)
+new_l = organize_lenses(l)
 
+print(total_power(new_l))
 
-
-
-
->>>>>>> Stashed changes
 
 # if __name__ == '__main__':
+
+#     l = to_input_list(f)
     
-#     print(total(to_input_list(f)))
+#     # part 1
+#     print(total(f))
