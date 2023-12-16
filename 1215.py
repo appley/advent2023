4aac7994a5806155113f4ea218cb73f3733703a2
pomme <copacabana@gmail.com>
2023-12-15 17:21:30 -0800

    Revert "working list construction"
    This reverts commit a5633a505c067493f0c3819288e729c6313dc872.

--- 1215.py
+++ 1215.py
@@ -1,6 +1,6 @@
 f = open("input/1215.txt", "r")
 
-TEST = ['rn=1', 'cm-', 'qp=3',  'cm=2', 'qp-', 'pc=4', 'ot=9', 'ab=5', 'pc-', 'pc=6', 'ot=7']
+TEST = ['rn=1'] # , 'cm-', 'qp=3', 'cm=2', 'qp-', 'pc=4', 'ot=9', 'ab=5', 'pc-', 'pc=6', 'ot=7']
 
 # part 1
 def to_input_list(f):
@@ -36,106 +36,83 @@ def total(input_list):
 # part 2
 
 def get_box_id(chars):
+
     s = chars.split("-")[0]
+
     return (hash(s), s)
 
 
 def focal_length(chars):
+
     s = chars.split("=")
+
     return (hash(s[0]), ((s[0]), int(s[1])))
 
 
-def handle_focal_length(inst,  box):
-
-    for i, j in enumerate(box):
-        print("hi", inst[1], j)
-        if inst[1] in j:
-            box[i] = ""
-            cp = box.copy()
-            box = slide_lenses(cp)
-        return box
-            
-        
-
 def slide_lenses(box):
-            
+
+    box.reverse()
     for i, j in enumerate(box):
         if j == "":
             continue
         else:
             curr = i
             prev = curr - 1
-            print("curr: ", curr, "prev: ", prev, box)
-            while curr != 0 and box[prev] == "":
+
+            while curr > 0 and box[prev] == "":
                 box[prev] = j
                 box[curr] = ""
                 curr = curr - 1
-                prev = curr - 1  
+                prev = curr - 1
+    
+    box.reverse()
     return box
 
-
-# TEST_BOX = ["", "rn=1", "acd", "", "abc", "", ""]
-
-# print(handle_focal_length("rn=1", TEST_BOX))
+TEST_BOX = [0, "rn=1", 0, 0, "abc", 0, 0]
 
 
-def insert_lens(lens, inst, box):
+def insert_lens(lens, box):
 
-    if all(i == "" for i in box):
+    if all(i == 0 for i in box):
         box[0] = lens
         return box
-    
     else:
         for i, j in enumerate(box):
-            if inst[1][0] in j:
-                box[i] = lens
-                cp = box.copy()
-                box = slide_lenses(cp)
-                
+            print("examining index ", i, j)
+            if j == 0:
+                continue
+            else:
+                box[i-1] = lens
                 return box
-    
-        box.append(lens)
-        cp = box.copy()
-        box = slide_lenses(cp)
-        return box
+
 
 def organize_lenses(input_list):
 
-    boxes = [[""]] * 256
-    print("origibal ", boxes)
+    boxes = [""] * 256
     # i will be raw chars
     for i in input_list:
 
         if "=" in i:
-            print("lens ", i)
             # box: label, focal length
             # (0, ('rn', 1))
             inst = focal_length(i)
-            print("focal length ", inst)
-            print(boxes[inst[0]])
-            print(boxes)
-            box_state = boxes[inst[0]].copy()
-            boxes[inst[0]] = insert_lens(i, inst, box_state)
+            if inst[1][0] in boxes[inst[0]]:
+                boxes[inst[0]] = i
+            else:
+                boxes = insert_lens(i, boxes)
 
         # handle box shifting
         else:
-            print("examing box id", i)
             # box: label
             # (1, 'qp')
             inst = get_box_id(i)
-            box_state = boxes[inst[0]].copy()
-            print("box id ", inst)
-            boxes[inst[0]] = handle_focal_length(inst, box_state)
+            if inst[1] in boxes[inst[0]]:
+                boxes[inst[0]] = ""
+                boxes = slide_lenses(boxes)
 
     return boxes
 
 print(organize_lenses(TEST))
-
-
-def power(box, slot, fl):
-
-
-
 
 
 
