d1e05b1413ee1a3204525a7dcba007ae824af3b9
pomme <copacabana@gmail.com>
2023-12-16 17:28:47 -0800

    working code

--- 1215.py
+++ 1215.py
@@ -1,7 +1,5 @@
 f = open("input/1215.txt", "r")
 
-TEST = ['rn=1', 'cm-', 'qp=3',  'cm=2', 'qp-', 'pc=4', 'ot=9', 'ab=5', 'pc-', 'pc=6', 'ot=7']
-
 
 def to_input_list(f):
 
@@ -52,54 +50,29 @@ def handle_focal_length(inst,  box):
     for i, j in enumerate(box):
         if inst[1] in j:
             box.remove(j)
-            # box[i] = ""
-            # cp = box.copy()
-            # box = slide_lenses(cp)
-        return box
-        
 
-def slide_lenses(box):
-            
-    for i, j in enumerate(box):
-        if j == "":
-            continue
-        else:
-            curr = i
-            prev = curr - 1
-            while curr != 0 and box[prev] == "":
-                box[prev] = j
-                box[curr] = ""
-                curr = curr - 1
-                prev = curr - 1  
     return box
 
 
-# TEST_BOX = ["", "rn=1", "acd", "", "abc", "", ""]
-
-# print(handle_focal_length("rn=1", TEST_BOX))
-
 def insert_lens(lens, inst, box):
 
-    if all(i == "" for i in box):
-        box[0] = lens
+    if len(box) == 0:
+        box.append(lens)
         return box
     
     else:
         for i, j in enumerate(box):
             if inst[1][0] in j:
-                box[i] = lens
-                # cp = box.copy()
-                # box = slide_lenses(cp)               
+                box[i] = lens     
                 return box
     
         box.append(lens)
-        # cp = box.copy()
-        # box = slide_lenses(cp)
         return box
+
 
 def organize_lenses(input_list):
 
-    boxes = [[""]] * 256
+    boxes = [[]] * 256
     # i will be raw chars
     for i in input_list:
 
@@ -110,11 +83,11 @@ def organize_lenses(input_list):
             box_state = boxes[inst[0]].copy()
             boxes[inst[0]] = insert_lens(i, inst, box_state)
 
-        # handle box shifting
         else:
             # box: label
             # (1, 'qp')
             inst = get_box_id(i)
+            print(inst, boxes[inst[0]])
             box_state = boxes[inst[0]].copy()
             boxes[inst[0]] = handle_focal_length(inst, box_state)
 
@@ -127,23 +100,19 @@ def total_power(boxes):
 
     for box_id, box in enumerate(boxes):
         for slot, lens in enumerate(box):
-            if lens != '':
-                power = (box_id + 1) * (slot + 1) * focal_length(lens)[1][1]
-                total = total + power
-        print("totalling: ", box_id, box, total)
+            power = (box_id + 1) * (slot + 1) * focal_length(lens)[1][1]
+            total = total + power
 
     return total
 
 
-l = to_input_list(f)
-new_l = organize_lenses(l)
 
-print(total_power(new_l))
+if __name__ == '__main__':
 
-
-# if __name__ == '__main__':
-
-#     l = to_input_list(f)
+    l = to_input_list(f)
     
-#     # part 1
-#     print(total(f))
+    # part 1
+    print(total(l))
+
+    # part 2
+    print(total_power(organize_lenses(l)))
