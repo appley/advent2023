de3a0a029d727ef50b00c5ce2d5c34badfd8d45d
pomme <copacabana@gmail.com>
2023-12-27 13:18:12 -0800

    inbounds soln

--- 1218.py
+++ 1218.py
@@ -56,22 +56,21 @@ def dig_u(m, row, col, num):
 
 def dig(plan):
 
-    # m = [["." for _ in range(500)] for _ in range(5000)]
+    m = [["." for _ in range(500)] for _ in range(5000)]
 
-    m = [["." for _ in range(10)] for _ in range(10)]
+    # m = [["." for _ in range(10)] for _ in range(10)]
 
-    curr_row = 0
-    curr_col = 0
+    # curr_row = 0
+    # curr_col = 0
 
-
-    # curr_row = 400
-    # curr_col = 70
+    curr_row = 400
+    curr_col = 70
 
     count = 1
     for p in plan:
         meters = p[1]
         # print("count", count)
-        print(p)
+        # print(p)
         # print("turning ", p[0], "from ", curr_row, curr_col)
         if p[0] == "R":
             m = dig_r(m, curr_row, curr_col, meters)
@@ -96,6 +95,7 @@ def dig(plan):
     return m
 
 
+
 def fill_row(row, start, stop):
 
     # print(start, stop)
@@ -106,59 +106,116 @@ def fill_row(row, start, stop):
     return row
 
 
+def is_inbounds(coord, map):
 
+    row = coord[0]
+    col = coord[1]
 
-
-def fill(row):
-
-    print(row)
-
-    indices = []
-
-    start = 0
-    stop = 0
-
-    for i, j in enumerate(row):
-
-        print("current start stop", (start, stop)) 
-        print(i, j)
-
-        count = 1
-       
-        # if i == stop:
-        #     continue
-
-        if j == "#" and start != -1:
-            print("setting new start", i)
-            start = i
+    def left():
+        if "#" in map[row][:col]:
+            # print("in bounds r")
+            return True
         
-        else:         
-
-            while i + count < len(row) and row[i+count] != "#":
-                
-                count = count + 1
+    def right():
+        if "#" in map[row][col:len(map[row])]:
+            return True
+        
+    def up():
+        for r in map[:row]:
+            if "#" in r[col]:
+                return True
+    
+    def down():
+        for r in map[row+1:]:
+            if "#" in r[col]:
+                return True
             
-            # stop = count + i
-            # indices.append((start, stop))
-                # print("looking at #s", row[i:len(row)-1])
+    
+    if left() and right() and up() and down():
+        return True
+    
+    else:
+        return False
+    
 
 
-            if "#" in row[i+1:len(row)-1]:
+def fill(map):
 
-                stop = i + count
-                print("stopping ", stop)
-                indices.append((start, stop))
-                # fill(row[i+1:len(row)-1])
+    for row_num, row in enumerate(map):
+        cp = row.copy()
+    
+        for col_num, col in enumerate(cp):
 
+            if col == ".":
+                coord = (row_num, col_num)
+                if is_inbounds(coord, map):
+                    cp[col_num]= "#"
+
+        map[row_num] = cp
+
+
+    return map
+
+    
+
+# def fill(row):
+
+#     print(row)
+
+#     indices = []
+
+#     start = 0
+#     stop = 0
+#     last = ("","")
+
+#     for i, j in enumerate(row):
+
+#         print("current start stop", (start, stop)) 
         
-    print(indices)
-    if len(indices) != 0:
 
-        for t in indices:
-            cp = row.copy()
-            row = fill_row(cp, t[0], t[1])
+#         count = 1
+#         print(i, j, count)
+       
 
-    return row
+#         if j == "#":
+#             print("setting new start", i)
+#             start = i + 1
+
+#             if i == last[1]:
+
+#                 print("skipping index ", i)
+#                 continue
+
+#             else:
+
+#                 while i + count < len(row) and row[i+count] != "#":
+                    
+#                     count = count + 1
+
+#                     print("curr stop", stop, count)
+                
+#                 # stop = count + i
+                    
+#                 # indices.append((start, stop))
+#                     # print("looking at #s", row[i:len(row)-1])
+
+
+#                 if "#" in row[i+1:len(row)]:
+
+#                     stop = i + count
+#                     print("setting stop ", stop)
+#                     last = (start, stop)
+#                     indices.append(last)
+ 
+        
+#     print(indices)
+#     if len(indices) != 0:
+
+#         for t in indices:
+#             cp = row.copy()
+#             row = fill_row(cp, t[0], t[1])
+
+#     return row
 
 
 
@@ -166,57 +223,23 @@ r = ['.', '#', '.', '#', '.', '.', '#', '.', '#', '.', '#', '.', '.']
 
 x = ['#', '#', '.', '.', '#', '#', '#', '.', '.', '.']
 
-print(fill(r))
 
-def fill2(row):
+y = ['#', '#', '.', '.', '#', '#', '#', '.', '.', '.']
 
-    indices = []    
 
-    for i, j in enumerate(row):
-
-        # print("considering ,", i, j)
-        # print("indices", indices)
-
-        # if j == "#" and row[i+1] == ".":
-        #     indices.append(i) # start index
-        #     count = 1
-        #     print(i + 1 + count)
-        #     while row[i+count] == "." and i + count < len(row) - 1:
-        #         count = count + 1
-        #     indices.append(i+count)
-
-        count = 1
-        if j == "#": # and i + count < len(row) - 1:
-            indices.append(i)
-            if len(row) - 1 > i + count and row[i+count] == ".":
-                count = count + 1
-                continue
-    
-    print(indices)
-    if len(indices) != 0:
-
-        for i, j in enumerate(indices[0:len(indices)-1]):
-
-            if indices[i+1] - j != 1:
-                cp = row.copy()
-                row = fill_row(cp, j, indices[i+1])
-
-    return row
+# print(fill(y))
 
 
 
+# def fill_map(map):
 
+#     for row_num, row in enumerate(map):
+#         cp = row.copy()
+#         map[row_num]= fill(cp)
 
+#     # print(map)
 
-def fill_map(map):
-
-    for row_num, row in enumerate(map):
-        cp = row.copy()
-        map[row_num]= fill(cp)
-
-    # print(map)
-
-    return map
+#     return map
 
 def total(map):
 
@@ -257,7 +280,7 @@ def print_map(map):
 
 
 
-# filled_map = fill_map(dig(get_instructions(t)))
+# filled_map = fill_map(dig(get_instructions(f)))
 # m = total(filled_map)
 # print(m)
 
@@ -266,6 +289,12 @@ def print_map(map):
 # m = to_string(fill_map(dig(get_instructions(t))))
 # m = fill_map(dig(get_instructions(t)))
 # print_map(m)
+
+
+m = fill(dig(get_instructions(f)))
+print_map(to_string(m))
+
+# print(total(m))
 
 
 
