b0dac5343f0b7c8dba1714b4f93a36c08771f732
pomme <copacabana@gmail.com>
2023-12-27 14:37:22 -0800

    cleaning code

--- 1206.py
+++ 1206.py
@@ -13,21 +13,19 @@ def calculate_num_wins(duration, winning_distance):
     for i in range (duration + 1):
 
         considered_dist = (duration - i) * i
-        # print("considering: ", i,  considered_dist)
         if considered_dist > winning_distance:
             total = total + 1
 
-    print(total)
     return total
 
-# print(
-#     calculate_num_wins(
-#         60, 601) * calculate_num_wins(
-#             80, 1163) * calculate_num_wins(
-#                 86, 1558) * calculate_num_wins(76, 1300))
+
+# part 1
+print(
+    calculate_num_wins(
+        60, 601) * calculate_num_wins(
+            80, 1163) * calculate_num_wins(
+                86, 1558) * calculate_num_wins(76, 1300))
     
 
 # part 2
-calculate_num_wins(60808676, 601116315591300)
-
-    
+print(calculate_num_wins(60808676, 601116315591300))
