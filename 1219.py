6f688fc85a769d44cbb6ac9da2968349f0f59337
pomme <copacabana@gmail.com>
2024-01-02 15:40:55 -0800

    working code but answer isn't correct

--- 1219.py
+++ 1219.py
@@ -117,10 +117,7 @@ def test_part(part, rule_tuple):
     return False
 
 
-def accepted_letter_combos(rule_tuple):
-
-    min = 1
-    max = 4000
+def accepted_letter_combos(rule_tuple, curr):
 
     letter = rule_tuple[0][0]  # handle letter dest
     test = rule_tuple[0][1]
@@ -129,15 +126,24 @@ def accepted_letter_combos(rule_tuple):
 
     if test == ">":
         if dest == "R":
-            return (letter, num - min + 1)
+            if curr > num:
+                return (letter, num - 1)
+            return (letter, curr)     
+
         else:
-            return (letter, max - num + 1)
+            if curr < num:
+                return (letter, 0)
+            return (letter, curr - num - 1)
         
-    if test == "<":
-        if dest == "R":            
-            return (letter, max - num + 1)
+    if test == "<":        
+        if dest == "R":      
+            if curr < num:
+                return (letter, 0)
+            return (letter, curr - num - 1)     
         else:
-            return (letter, num - min + 1)        
+            if curr > num:
+                return (letter, num - 1)
+            return (letter, curr)        
 
 
 def read_workflow(workflow, part):
@@ -155,6 +161,7 @@ def read_workflow(workflow, part):
         dest = r[1]
 
         if r[0] == 0 and dest == "R":
+            print("returning rejected")
             return combos
 
         elif r[0] == 0:
@@ -162,27 +169,32 @@ def read_workflow(workflow, part):
             continue
 
         elif dest == "R":
-            a = accepted_letter_combos(r)
+            letter = r[0][0]
+            a = accepted_letter_combos(r, part[letter])
             new_part = part.copy()
             # check this formula
-            new_part[a[0]] = part[a[0]] - a[1]
+            new_part[a[0]] = a[1]
             part = new_part
             continue
 
         else:
-            # append rule and inverse of rule
+            # append rule and continue w inverse of rule
+            letter = r[0][0]
             orig_part = part.copy()
             
-            a = accepted_letter_combos(r)
-            part[a[0]] = a[1]
-            combos.append((dest, part))
-            
-            # check this formula
-            orig_part[a[0]] = orig_part[a[0]] - a[1]
-            part = orig_part
-            continue
+            a = accepted_letter_combos(r, part[letter])
+            if a[1] == 0:
+                continue
+            else:
+                part[a[0]] = a[1]
+                combos.append((dest, part))
+                
+                # check this formula
+                orig_part[a[0]] = orig_part[a[0]] - a[1]
+                part = orig_part
+                continue
     
-    print("returning combos from workflow", combos)
+    print("returning combos from workflow", workflow, combos)
     return combos
 
 
@@ -198,9 +210,20 @@ p = {
 
 s = "in"
 
+
+p2 = {'x': 4000, 'm': 1801, 'a': 4000, 's': 1418}
+
+
+p3 =  {'x': 2584, 'm': 4000, 'a': 2006, 's': 1351}
+
+p4 = {'x': 4000, 'm': 2090, 'a': 1994, 's': 1351}
+
+p5 = {'x': 4000, 'm': 4000, 'a': 4000, 's': 2649}
+
+
 # print(read_workflow("in", p))
 
-print(read_workflow("crn", p))
+# print(read_workflow("qqz", p5))
 
 # part 1
 ####
@@ -214,7 +237,7 @@ def process_parts(part, start):
     print("PART", part)
 
     combos = read_workflow(start, part)
-    print("starting next with :", combos)
+    print("starting combos :", combos)
 
     for combo in combos:
         print("combo", combo)
@@ -226,37 +249,40 @@ def process_parts(part, start):
         cp = combo
 
         if cp[0] == "A":
+            print("APPENDING FROM call", cp, parts)
             parts.append(cp[1])
-        
+ 
         else:
-            while cp[0] != "A":
-                s = cp[0]
-                np = cp[1]
-                parts.append(process_parts(np, s))
+            s = cp[0]
+            np = cp[1]
+            new_parts = parts.copy()
+            new_parts.append(process_parts(np, s))
+            parts = new_parts
 
-        print("parts", parts)
-       
-
+    print("parts", parts)
     return parts
 
 print(process_parts(p, s))
+
+
+# combo qkq ('A', {'x': 1416, 'm': 4000, 'a': 2006, 's': 1351})
+
+ps = [{'x': 1415, 'm': 4000, 'a': 2005, 's': 1350}, {'x': 4000, 'm': 1909, 'a': 1995, 's': 1350}, {'x': 2439, 'm': 2091, 'a': 1995, 's': 814}, {'x': 4000, 'm': 961, 'a': 4000, 's': 2650}, {'x': 4000, 'm': 839, 'a': 1715, 's': 2650}]
+
 
 def total(parts):
 
     total = 0
 
     for p in parts:
-        pt = 0
+        combos = 1
         for _, v in p.items():
-            pt = pt + v
-        total = total + pt
+            combos = combos * v        
+        total = total + combos
 
     return total
 
-
-
-
-
+print(total(ps))
 
 
 
